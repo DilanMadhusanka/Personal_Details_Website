d426a61b18cdac6f791739c1ef37f624b91eb52f
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-15 22:06:10 +0530

    create skills webpage

--- my-profile/src/components/ContactMe.js
+++ my-profile/src/components/ContactMe.js
@@ -31,12 +31,6 @@ const ContactUs = () => {
                         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d638.1802295619985!2d80.62081808463283!3d6.608769078437658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1584249329346!5m2!1sen!2slk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> */}
                     </div>
                 </div>
-                {/* <div class="item">
-                    <i class="linkify icon"></i>
-                    <div class="content">
-                        <a href="http://dilan.madhusanka289@gmail.com">semantic-ui.com</a>
-                    </div>
-                </div> */}
             </div>
         </div>
         <SocialMedia />
--- my-profile/src/components/Skills.js
+++ my-profile/src/components/Skills.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import springboot from '../images/springboot.png';
+import react from '../images/react.png';
+import flutter from '../images/flutter.png';
 
 const Skills = () => {
-    return <div>Skills</div>
+    return (
+        <div className="ui text container">
+            <h2 className="ui icon header" style={{marginLeft:"100px"}}>
+                <i className="globe yellow icon" ></i>
+                <div class="content" style={{ fontSize:"40px"}}>
+                    Frameworks
+                    <div className="sub header" style={{color:"white"}}>I have more knowlegde about this frameworks.</div>
+                </div>
+            </h2>
+            <div className="ui vertical segment">
+                {/* <p>asdfkdjfkjdkfjkd fdjkfdjksjksla</p> */}
+                <img class="ui small rounded image" src={springboot} alt="spring boot" ></img>
+            </div>
+            <div className="ui vertical segment">
+                {/* <p>ddfldkfls</p> */}
+                <img class="ui small rounded image" src={react} alt="react" ></img>
+            </div>
+            <div class="ui vertical segment">
+                {/* <p>hg</p> */}
+                <img class="ui small rounded image" src={flutter} alt="flutter" ></img>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default Skills;
