5ab35571f7050b610db9a38cdddbf59e1c371bed
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-15 23:09:39 +0530

    change the navigation bar style

--- my-profile/src/components/NavBar.js
+++ my-profile/src/components/NavBar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
     render() {
+        const style = { fontSize:"18px" }
         return(
-            <div className="ui four item menu">
-                <Link to="/" className="item">Home</Link>
-                <Link to="/education" className="item">Education</Link>
-                <Link to="/skills" className="item">Skills</Link>
-                <Link to="/contacts" className="item">Contact Us</Link>
+            <div className="ui inverted menu" style={{height:"50px"}}>
+                <Link to="/" className="red item active" style={style}>Home</Link>
+                <Link to="/education" className="item" style={style}>Education</Link>
+                <Link to="/skills" className="item" style={style}>Skills</Link>
+                <Link to="/contacts" className="item" style={style}>Contact Us</Link>
             </div>
         );
     }
