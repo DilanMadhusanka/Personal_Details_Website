094491ea60e09b791626082e0a9cf1a6ad1cf9de
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-16 23:28:35 +0530

    add some styles to skills web page

--- my-profile/src/components/Skills.js
+++ my-profile/src/components/Skills.js
@@ -4,46 +4,55 @@ import react from '../images/react.png';
 import flutter from '../images/flutter.png';
 
 const Skills = () => {
+    const styleName = {
+        marginLeft:"50px",
+        fontSize:"40px",
+        // color:"darkblue"
+    }
+    const styleButton = {
+        marginTop:"60%"
+    }
     return (
-        <div className="ui text container">
+        <div className="ui container">
+            <div className="ui text container">
             <h2 className="ui icon header" style={{ marginLeft: "100px" }}>
-                <i className="globe yellow icon" ></i>
-                <div class="content" style={{ fontSize: "40px" }}>
+                <i className="globe yellow icon" style={{fontSize:"100px"}}></i>
+                <div class="content" style={{ fontSize: "50px" }}>
                     Frameworks
-                    <div className="sub header" style={{ color: "white" }}>I have more knowlegde about this frameworks.</div>
+                    <div className="sub header" style={{ color: "white", marginTop:"10px" }}>I have more knowlegde about this frameworks.</div>
                 </div>
             </h2>
+            </div>
 
             <div class="ui middle aligned divided list">
                 <div class="item">
                     <div class="right floated content">
-                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/LearningSpring", "_blank")}>Follow</div>
+                        <div class="ui button" style={styleButton} onClick={() => window.open("https://github.com/DilanMadhusanka/LearningSpring", "_blank")}>Follow</div>
                     </div>
                     <img class="ui small image" src={springboot} alt="img" />
-                    <div class="content">
+                    <div class="content" style={styleName}>
                         Springboot
-                </div>
+                    </div>
                 </div>
                 <div class="item">
                     <div class="right floated content">
-                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/React_Apps", "_blank")}>Follow</div>
+                        <div class="ui button" style={styleButton} onClick={() => window.open("https://github.com/DilanMadhusanka/React_Apps", "_blank")}>Follow</div>
                     </div>
                     <img class="ui small image" src={react} alt="img" />
-                    <div class="content">
+                    <div class="content" style={styleName}>
                         React
-                </div>
+                    </div>
                 </div>
                 <div class="item">
                     <div class="right floated content">
-                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/Flutter_App", "_blank")}>Follow</div>
+                        <div class="ui button" style={styleButton} onClick={() => window.open("https://github.com/DilanMadhusanka/Flutter_App", "_blank")}>Follow</div>
                     </div>
                     <img class="ui small image" src={flutter} alt="img" />
-                    <div class="content">
+                    <div class="content" style={styleName}>
                         Flutter
-                </div>
+                    </div>
                 </div>
             </div>
-
 
         </div>
     );
