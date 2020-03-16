7bcf5585ce193e67575f27eb518471eb817a5b2d
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-16 23:03:01 +0530

    change the structure of skill web page and add follow button to see simple practices of framewok

--- my-profile/src/components/Skills.js
+++ my-profile/src/components/Skills.js
@@ -6,24 +6,42 @@ import flutter from '../images/flutter.png';
 const Skills = () => {
     return (
         <div className="ui text container">
-            <h2 className="ui icon header" style={{marginLeft:"100px"}}>
+            <h2 className="ui icon header" style={{ marginLeft: "100px" }}>
                 <i className="globe yellow icon" ></i>
-                <div class="content" style={{ fontSize:"40px"}}>
+                <div class="content" style={{ fontSize: "40px" }}>
                     Frameworks
-                    <div className="sub header" style={{color:"white"}}>I have more knowlegde about this frameworks.</div>
+                    <div className="sub header" style={{ color: "white" }}>I have more knowlegde about this frameworks.</div>
                 </div>
             </h2>
-            <div className="ui vertical segment">
-                {/* <p>asdfkdjfkjdkfjkd fdjkfdjksjksla</p> */}
-                <img class="ui small rounded image" src={springboot} alt="spring boot" ></img>
-            </div>
-            <div className="ui vertical segment">
-                {/* <p>ddfldkfls</p> */}
-                <img class="ui small rounded image" src={react} alt="react" ></img>
-            </div>
-            <div class="ui vertical segment">
-                {/* <p>hg</p> */}
-                <img class="ui small rounded image" src={flutter} alt="flutter" ></img>
+
+            <div class="ui middle aligned divided list">
+                <div class="item">
+                    <div class="right floated content">
+                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/LearningSpring", "_blank")}>Follow</div>
+                    </div>
+                    <img class="ui small image" src={springboot} alt="img" />
+                    <div class="content">
+                        Springboot
+                </div>
+                </div>
+                <div class="item">
+                    <div class="right floated content">
+                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/React_Apps", "_blank")}>Follow</div>
+                    </div>
+                    <img class="ui small image" src={react} alt="img" />
+                    <div class="content">
+                        React
+                </div>
+                </div>
+                <div class="item">
+                    <div class="right floated content">
+                        <div class="ui button" onClick={() => window.open("https://github.com/DilanMadhusanka/Flutter_App", "_blank")}>Follow</div>
+                    </div>
+                    <img class="ui small image" src={flutter} alt="img" />
+                    <div class="content">
+                        Flutter
+                </div>
+                </div>
             </div>
 
 
