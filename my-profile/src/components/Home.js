0911cce97805ef84a83c1952be472a9c86dff089
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-16 22:37:33 +0530

    change the style of the header in the Home page

--- my-profile/src/components/Home.js
+++ my-profile/src/components/Home.js
@@ -3,18 +3,27 @@ import profile from '../images/profile.jpg'
 import SocialMedia from './SocialMedia';
 
 const Home = () => {
+    const style = {
+        backgroundColor: "lightblue", fontSize: "50px", fontStyle: "italic", fontFamily: "New Times Roman",
+        color: "purple"
+    }
     return (
         <div className="ui container">
-            <h1 className="ui center aligned icon header" style={{color:"black", fontSize:'50px'}}>
-            <img class="ui medium circular image" src={profile} style={{width:"200px", height:"200px"}} alt="img" />
-                {/* <i class="circular users icon"></i> */}
-                I am Dilan Madhusanka
+
+            <div className="ui raised segment" style={{backgroundColor:"lightblue"}}>
+            <h1 class="ui block header" style={style}>
+                <img class="ui medium circular image" src={profile} style={{ width: "200px", height: "170px" }} alt="img" />
+                <div class="content" style={{marginLeft:"20px"}}>
+                    I am Dilan Madhusanka
+                    <div class="sub header">Bsc. Honz Software Engineering, University of Kelaniya</div>
+                </div>
             </h1>
+            </div>
 
             <div class="ui raised very padded text container segment">
                 {/* <h2 class="ui header">Dogs Roles with Humans</h2> */}
                 <p>I am undergraduate of Software Engineering in University of Kelaniya. Burn on 13th of November in 1996 and
-                    home town is Rathnapura. I have followed the subjects as full stack developer about web and mobile.
+                home town is Rathnapura. I have followed the subjects as full stack developer about web and mobile.
                 </p>
             </div> <br />
             <SocialMedia />
